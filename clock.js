bc896e50547aa5cd82a4bd41bc3f21980a6ea455
Becky Hamm <becky@metricslice.com>
2015-02-09 15:29:38 -0500

    changed zeros adding if statements to a function that could be reused for each part of the assignment

--- clock.js
+++ clock.js
@@ -4,52 +4,49 @@ var counter = function() {
     var minutes = date.getMinutes();
     var hours = date.getHours();
 
-    // add 0's if needed
-    if (seconds.toString().length < 2) { 
-        seconds = "0" + seconds;
-    }
-    if (minutes.toString().length < 2) {
-        minutes = "0" + minutes;
-    }
-    if (hours.toString().length < 2) {
-        hours = "0" + hours;
-    }
+    // add 0's if needed; changed this to a function to make my code more DRY
+    var addZeros = function(arr) {
+        var newArray = [];
+        arr.forEach(function(item) {
+            if (item.toString().length < 2) {
+                item = "0" + item;
+            }
+            newArray.push(item);
+        });
+        return newArray;
+    };
 
     var time = [hours, minutes, seconds];
 
+
+
+
     // Part 1
-    var clock = time.join(":");
+    var clock = addZeros(time).join(":");
 
     $(".clock").text(clock); // at first this wasn't repeating because I had this outside the function
 
 
+
+
     // Part 2: add a # and take out the :, then put that hex code in the background of the page
-    var hexClock = "#" + ([hours, minutes, seconds].join(""));
+    var hexClock = "#" + (addZeros(time).join(""));
 
     $(".hex-clock").text(hexClock);
-
     $(".hex-clock-page").css("background-color", hexClock);
+
+
+
 
     // Part Go Further: switch seconds minutes and hours into base 16 to be a real hex code, then add to background as well
     var hexSeconds = seconds.toString(16).toUpperCase();
     var hexMinutes = minutes.toString(16).toUpperCase();
     var hexHours = hours.toString(16).toUpperCase();
 
-    // add 0's if needed
-    if (hexSeconds.toString().length < 2) { 
-        hexSeconds = "0" + hexSeconds;
-    }
-    if (hexMinutes.toString().length < 2) {
-        hexMinutes = "0" + hexMinutes;
-    }
-    if (hexHours.toString().length < 2) {
-        hexHours = "0" + hexHours;
-    }
-
-    var hexCode = [hexHours, hexMinutes, hexSeconds].join("");
+    var hexTime = [hexHours, hexMinutes, hexSeconds];
+    var hexCode = addZeros(hexTime).join("");
 
     $(".hex-code").text(hexCode);
-
     $(".hex-code-page").css("background-color", ("#" + hexCode));
 
 }
@@ -58,7 +55,5 @@ var counter = function() {
 $(function(){
 
     setInterval(counter, 1000);
-
-    //counter();
 
 });
